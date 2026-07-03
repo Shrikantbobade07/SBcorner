d49de37f79b91ee6add22a4540086b7cb7e57127
SBcorner <shrikantbobade07@gmail.com>
2026-07-03 15:57:55 +0530

    remove duplicate in list

--- python practies/ex6.py
+++ python practies/ex6.py
@@ -1,1 +1,15 @@
-#code file check
+import pandas as pd
+
+data = {
+    'A' : [1,2,2,3,4,3,5],
+    'B' : ['x','y','z','x','z','w','v']
+}
+
+df = pd.DataFrame(data)
+
+remove_duplicate = df.drop_duplicates(keep='first') # compare row line by line
+remove_duplicate_A = df.drop_duplicates(subset=['A'], keep=False)
+
+print("clened (keep first)", remove_duplicate)
+print("cleand (based on A)", remove_duplicate_A)
+

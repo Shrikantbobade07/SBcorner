395a0ccae8b7ff21f41c8eea4dd04a8869192147
SBcorner <shrikantbobade07@gmail.com>
2026-07-03 15:03:00 +0530

    write a generator example

--- python practies/ex5.py
+++ python practies/ex5.py
@@ -1,1 +1,7 @@
-print ("hello just check pull request")
+def even_number(n):
+    for i in range(1, n+1):
+        if i%2 == 0:
+            yield i
+gen = even_number(10)
+for num in gen:
+    print(num)

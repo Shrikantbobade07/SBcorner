b5adea03c9739053a15a22ff0067627bbb1fe7ed
SBcorner <shrikantbobade07@gmail.com>
2026-07-13 10:38:25 +0530

    new add

---
+++ python practies/numpy_example.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+lit = [1,2,3,4]
+arr = np.array(lit)
+print(arr)
---
+++ python practies/numpy_example_1.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+lst = [1,2,3,4]
+lst1 = [4,5,6,7]
+lst2 = [4,6,7,8]
+
+arr = np.array([lst, lst1, lst2])
+print (arr)
+print (arr.shape)
